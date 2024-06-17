eac05de57f6a7530c023a538c050b83bc185e565
Upond <pond_chanawin@hotmail.com>
2024-06-17 10:43:57 +0700

    add rightsidebar

---
+++ app/(root)/tags/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+	return <div>Tag</div>;
+};
+
+export default page;
---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,106 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const hotQuestions = [
+	{
+		_id: 1,
+		title:
+			'Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?',
+	},
+	{
+		_id: 2,
+		title: 'Is it only me or the font is bolder than necessary?',
+	},
+	{
+		_id: 3,
+		title: 'Redux Toolkit Not Updating State as Expected',
+	},
+	{
+		_id: 4,
+		title: 'Can I get the course for free?',
+	},
+	{
+		_id: 5,
+		title: 'Async/Await Function Not Handling Errors Properly',
+	},
+];
+
+const popularTags = [
+	{
+		_id: 1,
+		tag: 'nextjs',
+		tagCount: 32,
+	},
+	{
+		_id: 2,
+		tag: 'test',
+		tagCount: 19,
+	},
+	{
+		_id: 3,
+		tag: 'react',
+		tagCount: 17,
+	},
+	{
+		_id: 4,
+		tag: 'css',
+		tagCount: 13,
+	},
+	{
+		_id: 5,
+		tag: 'next js',
+		tagCount: 9,
+	},
+];
+
+const RightSidebar = () => {
+	return (
+		<section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-l p-6 pt-36 shadow-light-200  dark:shadow-none max-xl:hidden lg:w-[350px]">
+			<div>
+				<h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+				<div className="mt-7 flex w-full flex-col gap-[30px]">
+					{hotQuestions.map((question) => (
+						<Link
+							href={`/questions/${question._id}`}
+							key={question._id}
+							className="flex cursor-pointer items-center justify-between gap-7"
+						>
+							<p className="body-medium text-dark500_light700">
+								{question.title}
+							</p>
+							<Image
+								src="/assets/icons/chevron-right.svg"
+								width={20}
+								height={20}
+								alt="chevron right"
+								className="invert-colors"
+							/>
+						</Link>
+					))}
+				</div>
+			</div>
+			<div className="mt-16">
+				<h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+				<div className="mt-7 flex w-full flex-col gap-[30px]">
+					{popularTags.map((tag) => (
+						<Link
+							key={tag._id}
+							href={`/tags/${tag._id}`}
+							className="flex items-center justify-between"
+						>
+							<p className="body-medium background-light800_dark300 text-light400_light500 rounded-md px-4 py-2">
+								{tag.tag.toUpperCase()}
+							</p>
+							<p className="body-medium text-dark500_light700">
+								{tag.tagCount}
+							</p>
+						</Link>
+					))}
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default RightSidebar;
