6026eda4ee0f3d6d588a2cbd4beb4d6712549ef8
Upond <pond_chanawin@hotmail.com>
2024-06-18 14:07:57 +0700

    add question card

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,53 @@
+import QuestionCard from '@/components/cards/QuestionCard';
 import HomeFilters from '@/components/home/HomeFilters';
 import Filter from '@/components/shared/Filter';
+import NoResult from '@/components/shared/NoResult';
 import LocalSearchbar from '@/components/shared/search/LocalSearchbar';
 import { Button } from '@/components/ui/button';
 import { HomePageFilters } from '@/constants/filter';
 import Link from 'next/link';
+
+const questions = [
+	{
+		_id: 1,
+		title:
+			'Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?',
+		tags: [
+			{
+				_id: '1',
+				name: 'nextjs',
+			},
+		],
+		author: {
+			_id: '1',
+			name: 'Sujata',
+			picture: '', // Assuming picture URL, leave it empty if not available
+		},
+		upVotes: 71,
+		answers: [], // Placeholder empty object, replace with actual answer objects
+		views: 780000,
+		createdAt: new Date('2021-09-01T12:00:00.000Z'), // Adjust to actual creation date
+	},
+	{
+		_id: 2,
+		title: 'How to center using flexbox?',
+		tags: [
+			{
+				_id: '1',
+				name: 'css',
+			},
+		],
+		author: {
+			_id: '2',
+			name: 'chanawin',
+			picture: '', // Assuming picture URL, leave it empty if not available
+		},
+		upVotes: 7,
+		answers: [], // Placeholder empty object, replace with actual answer objects
+		views: 78,
+		createdAt: new Date('2023-09-01T12:00:00.000Z'), // Adjust to actual creation date
+	},
+];
 
 export default function Home() {
 	return (
@@ -33,6 +77,33 @@ export default function Home() {
 			</div>
 
 			<HomeFilters />
+
+			<div className="mt-10 flex w-full flex-col gap-6">
+				{questions.length > 0 ? (
+					questions.map((question) => (
+						<QuestionCard
+							key={question._id}
+							_id={question._id}
+							title={question.title}
+							tags={question.tags}
+							author={question.author}
+							upVotes={question.upVotes}
+							answers={question.answers}
+							views={question.views}
+							createdAt={question.createdAt}
+						/>
+					))
+				) : (
+					<NoResult
+						title="There's no result to show"
+						description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+				discussion. our query could be the next big thing others learn from. Get
+				involved! 💡"
+						link="/ask-question"
+						linkTitle="Ask a Question"
+					/>
+				)}
+			</div>
 		</>
 	);
 }
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -5,51 +5,51 @@ import RenderTag from './RenderTag';
 
 const hotQuestions = [
 	{
-		_id: 1,
+		_id: '1',
 		title:
 			'Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?',
 	},
 	{
-		_id: 2,
+		_id: '2',
 		title: 'Is it only me or the font is bolder than necessary?',
 	},
 	{
-		_id: 3,
+		_id: '3',
 		title: 'Redux Toolkit Not Updating State as Expected',
 	},
 	{
-		_id: 4,
+		_id: '4',
 		title: 'Can I get the course for free?',
 	},
 	{
-		_id: 5,
+		_id: '5',
 		title: 'Async/Await Function Not Handling Errors Properly',
 	},
 ];
 
 const popularTags = [
 	{
-		_id: 1,
+		_id: '1',
 		tag: 'nextjs',
 		tagCount: 32,
 	},
 	{
-		_id: 2,
+		_id: '2',
 		tag: 'test',
 		tagCount: 19,
 	},
 	{
-		_id: 3,
+		_id: '3',
 		tag: 'react',
 		tagCount: 17,
 	},
 	{
-		_id: 4,
+		_id: '4',
 		tag: 'css',
 		tagCount: 13,
 	},
 	{
-		_id: 5,
+		_id: '5',
 		tag: 'next js',
 		tagCount: 9,
 	},
