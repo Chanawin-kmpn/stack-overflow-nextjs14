ad6c565eadadea7efea44db66df1bfe7ce19c2d9
Upond <pond_chanawin@hotmail.com>
2024-06-20 13:56:17 +0700

    git restore middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-	'/',
-	'/ask-question(.*)',
-	'/api/webhook',
-	'question/:id',
-	'/tags',
-	'/tags/:id',
-	'/profile/:id',
-	'/community',
-	'/jobs',
-]);
+const isProtectedRoute = createRouteMatcher(['/ask-question(.*)']);
 
 export default clerkMiddleware((auth, req) => {
 	if (isProtectedRoute(req)) auth().protect();
