96ebf86a79fcfa218d65fb7d5ede2423e398b523
Upond <pond_chanawin@hotmail.com>
2024-06-24 14:17:23 +0700

    add tag detail

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -2,8 +2,14 @@
 
 import User from '@/database/user.model';
 import { connectToDatabase } from '../mongoose';
-import { GetAllTagsParams, GetTopInteractedTagsParams } from './shared.teypes';
-import Tag from '@/database/tag.model';
+import {
+	GetAllTagsParams,
+	GetQuestionsByTagIdParams,
+	GetTopInteractedTagsParams,
+} from './shared.teypes';
+import Tag, { ITag } from '@/database/tag.model';
+import Question from '@/database/question.model';
+import { FilterQuery } from 'mongoose';
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
 	try {
@@ -37,5 +43,43 @@ export async function getAllTags(params: GetAllTagsParams) {
 	} catch (error) {
 		console.log(error);
 		throw new Error('No tag founded!');
+	}
+}
+
+export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
+	try {
+		connectToDatabase();
+
+		const { tagId, searchQuery, page = 1, pageSize = 10 } = params;
+
+		const tagFilter: FilterQuery<ITag> = { _id: tagId };
+
+		const tag = await Tag.findOne(tagFilter).populate({
+			path: 'questions',
+			model: Question,
+			match: searchQuery
+				? { title: { $regex: searchQuery, $options: 'i' } }
+				: {},
+			options: {
+				sort: { createdAt: -1 },
+			},
+			populate: [
+				{ path: 'tags', model: Tag, select: '_id name' },
+				{ path: 'author', model: User, select: '_id clerkId name picture' },
+			],
+		});
+		// populate อันนี้จะเป็นการดึงข้อมูลจาก path question ไม่ใช่จาก tag โดยตรง
+		if (!tag) {
+			console.log('Tag not found');
+		}
+
+		const questions = tag.questions;
+
+		// console.log(tag.questions);
+
+		return { tagTitle: tag.name, questions };
+	} catch (error) {
+		console.log(error);
+		throw error;
 	}
 }
